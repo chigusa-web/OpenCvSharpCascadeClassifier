86280d32281333f11db45d804a181e67d3be22b2
kimamacode <kimamacode@gmail.com>
2021-03-12 22:38:31 +0900

    プロジェクト ファイルを追加します。

---
+++ OpenCvSharpCascadeClassifier/fMain.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace OpenCvSharpCascadeClassifier
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbSrc = new System.Windows.Forms.PictureBox();
+            this.lblImage = new System.Windows.Forms.Label();
+            this.cbCascades = new System.Windows.Forms.ComboBox();
+            this.lblCascades = new System.Windows.Forms.Label();
+            this.pbOutput = new System.Windows.Forms.PictureBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSrc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbSrc
+            // 
+            this.pbSrc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbSrc.Location = new System.Drawing.Point(12, 91);
+            this.pbSrc.Name = "pbSrc";
+            this.pbSrc.Size = new System.Drawing.Size(563, 469);
+            this.pbSrc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSrc.TabIndex = 0;
+            this.pbSrc.TabStop = false;
+            this.pbSrc.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.pbSrc_LoadCompleted);
+            this.pbSrc.DragDrop += new System.Windows.Forms.DragEventHandler(this.pbSrc_DragDrop);
+            this.pbSrc.DragEnter += new System.Windows.Forms.DragEventHandler(this.pbSrc_DragEnter);
+            // 
+            // lblImage
+            // 
+            this.lblImage.AutoSize = true;
+            this.lblImage.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.lblImage.Location = new System.Drawing.Point(9, 72);
+            this.lblImage.Name = "lblImage";
+            this.lblImage.Size = new System.Drawing.Size(238, 16);
+            this.lblImage.TabIndex = 1;
+            this.lblImage.Text = "画像ファイルをドラッグ・アンド・ドロップ";
+            // 
+            // cbCascades
+            // 
+            this.cbCascades.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCascades.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.cbCascades.FormattingEnabled = true;
+            this.cbCascades.Location = new System.Drawing.Point(12, 29);
+            this.cbCascades.Name = "cbCascades";
+            this.cbCascades.Size = new System.Drawing.Size(513, 24);
+            this.cbCascades.TabIndex = 3;
+            // 
+            // lblCascades
+            // 
+            this.lblCascades.AutoSize = true;
+            this.lblCascades.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.lblCascades.Location = new System.Drawing.Point(9, 9);
+            this.lblCascades.Name = "lblCascades";
+            this.lblCascades.Size = new System.Drawing.Size(161, 16);
+            this.lblCascades.TabIndex = 4;
+            this.lblCascades.Text = "カスケード分類器を指定";
+            // 
+            // pbOutput
+            // 
+            this.pbOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbOutput.Location = new System.Drawing.Point(603, 91);
+            this.pbOutput.Name = "pbOutput";
+            this.pbOutput.Size = new System.Drawing.Size(563, 469);
+            this.pbOutput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbOutput.TabIndex = 5;
+            this.pbOutput.TabStop = false;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.lblOutput.Location = new System.Drawing.Point(600, 72);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(72, 16);
+            this.lblOutput.TabIndex = 6;
+            this.lblOutput.Text = "検出結果";
+            // 
+            // F_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 569);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.pbOutput);
+            this.Controls.Add(this.lblCascades);
+            this.Controls.Add(this.cbCascades);
+            this.Controls.Add(this.lblImage);
+            this.Controls.Add(this.pbSrc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "F_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OpenCvSharp 物体検出";
+            ((System.ComponentModel.ISupportInitialize)(this.pbSrc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbSrc;
+        private System.Windows.Forms.Label lblImage;
+        private System.Windows.Forms.ComboBox cbCascades;
+        private System.Windows.Forms.Label lblCascades;
+        private System.Windows.Forms.PictureBox pbOutput;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ OpenCvSharpCascadeClassifier/fMain.cs
@@ -0,0 +1,155 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenCvSharpCascadeClassifier
+{
+    public partial class fMain : Form
+    {
+
+        // カスケードファイルの保存ディレクトリ
+#if DEBUG
+        string dirCascades = @"..\..\..\cascades\";
+#else 
+        string dirCascades = @".\cascades\";
+#endif
+
+        // カスケードファイルのパスリスト
+        List<string> cascadePaths = new List<string>();
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public fMain()
+        {
+            InitializeComponent();
+
+            // ドロップを許可
+            pbSrc.AllowDrop = true;
+
+            // カスケードファイルの読み込み
+            ReadCascadesFile();
+        }
+
+        /// <summary>
+        /// カスケードファイルの読み込み
+        /// </summary>
+        private void ReadCascadesFile()
+        {
+            // ファイル一覧を取得する
+            string[] files = System.IO.Directory.GetFiles(dirCascades, "*.xml", System.IO.SearchOption.AllDirectories);
+
+            foreach (string filePath in files)
+            {
+                // ファイル名を分解
+                string fileName = Path.GetFileName(filePath);
+
+                // コンボボックスに追加
+                cbCascades.Items.Add(fileName);
+
+                // ファイルパスを保持
+                cascadePaths.Add(filePath);
+            }
+
+            if (cbCascades.Items.Count > 0)
+            {
+                // 一件目を選択
+                cbCascades.SelectedIndex = 0;
+            }
+
+        }
+
+        /// <summary>
+        /// PictureBox ドラッグイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pbSrc_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        /// <summary>
+        /// PictureBox ドロップイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pbSrc_DragDrop(object sender, DragEventArgs e)
+        {
+            // ドラッグされたファイルのパスを取得
+            string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            // 画像を表示
+            pbSrc.ImageLocation = fileName[0];
+
+        }
+
+        /// <summary>
+        /// 画像読み込み完了イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pbSrc_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            // 物体検出
+            Detect();
+        }
+
+        /// <summary>
+        /// 物体検出
+        /// </summary>
+        private void Detect()
+        {
+
+            // 画像が未読み込み、または、画像が無効な場合は処理なし
+            if (pbSrc.Image == null || pbSrc.Image == pbSrc.ErrorImage)
+            {
+                return;
+            }
+
+            // カスケードファイルの選択有無
+            if (cbCascades.SelectedIndex <= -1)
+            {
+                return;
+            }
+
+            // カスケードファイルパスを取得
+            string cascadePath = cascadePaths[cbCascades.SelectedIndex];
+
+            // PictureBoxの画像をBitmapで取得
+            using (var bmp = new Bitmap(pbSrc.Image))
+            // BitmapをMatに変換
+            using (var mat = OpenCvSharp.Extensions.BitmapConverter.ToMat(bmp))
+            // カスケード分類器の読み込み
+            using (var cascade = new CascadeClassifier(cascadePath))
+            {
+                // 検出
+                foreach (Rect rectDetect in cascade.DetectMultiScale(mat))
+                {
+                    // 顔に枠を表示
+                    Rect rect = new Rect(rectDetect.X, rectDetect.Y, rectDetect.Width, rectDetect.Height);
+                    Cv2.Rectangle(mat, rect, new OpenCvSharp.Scalar(255, 255, 0), 2);
+                }
+
+                // 画像を表示
+                pbOutput.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(mat.CvtColor(ColorConversionCodes.BGRA2BGR));
+            }
+        }
+
+    }
+}
